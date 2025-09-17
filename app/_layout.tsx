5d85708865d647a4f2adf56dd595f65e603d07de
Kain Nhantumbo <nhantumbok@gmail.com>
2025-09-17 22:43:45 +0200

    feat: added base styles config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,16 @@
+import { ThemeProvider } from "@/theme/theme-provider";
 import { Stack } from "expo-router";
 import "../styles/global.css";
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false
-      }}>
-      <Stack.Screen name='(tabs)/splash' />
-    </Stack>
+    <ThemeProvider>
+      <Stack
+        screenOptions={{
+          headerShown: false
+        }}>
+        <Stack.Screen name='(tabs)/splash' />
+      </Stack>
+    </ThemeProvider>
   );
 }
