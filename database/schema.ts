8dd997300bf6cb3a968ae5b752e6efbb4a8b4768
Kain Nhantumbo <nhantumbok@gmail.com>
2025-09-26 15:17:46 +0200

    feat: added synced prop to thr database for future sync support

--- database/schema.ts
+++ database/schema.ts
@@ -3,5 +3,7 @@ import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 export const tasks = sqliteTable("tasks", {
   id: integer("id").primaryKey({ autoIncrement: true }),
   title: text("title").notNull(),
-  done: integer("done", { mode: "boolean" }).default(false).notNull()
+  done: integer("done", { mode: "boolean" }).default(false).notNull(),
+  updatedAt: integer("updated_at").notNull(),
+  synced: integer("synced", { mode: "boolean" }).default(false).notNull()
 });
--- database/validations.ts
+++ database/validations.ts
@@ -3,7 +3,8 @@ import { z } from "zod";
 export const TaskSchema = z.object({
   id: z.number().optional(),
   title: z.string().min(1, "Task title cannot be empty"),
-  done: z.boolean().default(false)
+  done: z.boolean().default(false),
+  synced: z.boolean().default(false)
 });
 
 export type TaskSchemaType = z.infer<typeof TaskSchema>;
