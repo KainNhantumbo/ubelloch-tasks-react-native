e9a7b6a5f8e69527d992661c2af6c23435e569ff
Kain Nhantumbo <nhantumbok@gmail.com>
2025-09-27 10:43:47 +0200

    feat: added a prototype for calendar  page

---
+++ app/(home)/calendar/index.tsx
@@ -0,0 +1,12 @@
+import { Text } from "@/components/ui/text";
+import { Calendar1Icon } from "lucide-react-native";
+import { View } from "react-native";
+
+export default function RespectScreen() {
+  return (
+    <View className='flex-1 items-center justify-center bg-background'>
+      <Calendar1Icon size={48} color='#007AFF' className='mb-4' />
+      <Text className='mb-2 text-2xl font-bold'>Calendar</Text>
+    </View>
+  );
+}
