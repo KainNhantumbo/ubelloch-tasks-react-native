748f280f2917bde39425185905adfe002b5e04c8
Kain Nhantumbo <nhantumbok@gmail.com>
2025-09-28 17:58:35 +0200

    feat: added getNotesByFolderId helper function to folder data store

--- store/folders.ts
+++ store/folders.ts
@@ -1,6 +1,10 @@
 import { orm as db } from "@/database/client";
-import { folders } from "@/database/schema";
-import { FolderSchema, type FolderSchemaType } from "@/database/validations";
+import { folders, notes } from "@/database/schema";
+import {
+  FolderSchema,
+  NoteSchemaType,
+  type FolderSchemaType
+} from "@/database/validations";
 import { eq } from "drizzle-orm";
 import { create } from "zustand";
 
@@ -10,6 +14,7 @@ interface FoldersState {
   addFolder: (data: Omit<FolderSchemaType, "id">) => Promise<void>;
   updateFolder: (id: number, updates: Partial<FolderSchemaType>) => Promise<void>;
   deleteFolder: (id: number) => Promise<void>;
+  getNotesByFolderId: (folderId: number) => Promise<NoteSchemaType[]>;
 }
 
 export const useFoldersStore = create<FoldersState>((set, get) => ({
@@ -35,5 +40,10 @@ export const useFoldersStore = create<FoldersState>((set, get) => ({
   deleteFolder: async (id) => {
     await db.delete(folders).where(eq(folders.id, id));
     await get().fetchFolders();
+  },
+
+  getNotesByFolderId: async (folderId) => {
+    const data = await db.select().from(notes).where(eq(notes.folderId, folderId));
+    return data as NoteSchemaType[];
   }
 }));
