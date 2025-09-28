28015e7ee914015817a530755e04f4af3029cca7
Kain Nhantumbo <nhantumbok@gmail.com>
2025-09-28 21:41:31 +0200

    refactor: moved carousel component of onboarding screen to its own file

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,93 +1,15 @@
+import { CarouselContainer } from "@/components/onboarding-carousel";
 import { Button } from "@/components/ui/button";
 import { Text } from "@/components/ui/text";
 import { appBaseConfig } from "@/constants";
 import { THEME } from "@/lib/theme";
-import { useAssets } from "expo-asset";
 import { LinearGradient } from "expo-linear-gradient";
 import { Link } from "expo-router";
 import { SproutIcon } from "lucide-react-native";
 import { useColorScheme } from "nativewind";
 import * as React from "react";
-import { Image, StatusBar, View } from "react-native";
-import { useSharedValue } from "react-native-reanimated";
-import Carousel from "react-native-reanimated-carousel";
+import { StatusBar, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
-type OnboardCarouselItem = {
-  title: string;
-  description: string;
-  imagePath: string | undefined;
-};
-
-export function CarouselContainer() {
-  const progress = useSharedValue<number>(0);
-  const [assets, error] = useAssets([
-    require("../assets/onboarding/images/notebook.png"),
-    require("../assets/onboarding/images/organization.png"),
-    require("../assets/onboarding/images/user-vision.png")
-  ]);
-
-  const onboardCarouselItems: OnboardCarouselItem[] = [
-    {
-      title: "Track Your Tasks",
-      description:
-        "Easily manage and organize your daily tasks with our intuitive interface.",
-      imagePath: assets?.[0].uri
-    },
-    {
-      title: "Stay Organized",
-      description: "Categorize your tasks and set priorities to maximize productivity.",
-      imagePath: assets?.[1].uri
-    },
-    {
-      title: "Achieve More",
-      description:
-        "Boost your efficiency and accomplish your goals with our task management system.",
-      imagePath: assets?.[2].uri
-    }
-  ];
-
-  if (error) {
-    console.error(error);
-    return (
-      <View>
-        <Text>Error loading assets</Text>
-      </View>
-    );
-  }
-
-  return (
-    <Carousel
-      autoPlayInterval={4000}
-      data={onboardCarouselItems}
-      loop={true}
-      pagingEnabled={true}
-      snapEnabled={true}
-      autoPlay={true}
-      onProgressChange={progress}
-      width={400}
-      height={400}
-      renderItem={({ item, index }) => {
-        return (
-          <View key={index} className='-ml-6 mt-4 w-full flex-1 items-center'>
-            <Image
-              source={{ uri: item.imagePath }}
-              className='aspect-square h-[260px] w-[260px] object-cover'
-            />
-            <View className='p-8'>
-              <Text className='text-center font-sans text-3xl font-medium leading-relaxed'>
-                {item.title}
-              </Text>
-              <Text className='text-md w-[280px] text-center font-sans leading-8'>
-                {item.description}
-              </Text>
-            </View>
-          </View>
-        );
-      }}
-    />
-  );
-}
 
 export default function Onboarding() {
   const { colorScheme } = useColorScheme();
---
+++ components/onboarding-carousel.tsx
@@ -0,0 +1,82 @@
+import { Text } from "@/components/ui/text";
+import { useAssets } from "expo-asset";
+import * as React from "react";
+import { Image, View } from "react-native";
+import { useSharedValue } from "react-native-reanimated";
+import Carousel from "react-native-reanimated-carousel";
+
+type OnboardCarouselItem = {
+  title: string;
+  description: string;
+  imagePath: string | undefined;
+};
+
+export function CarouselContainer() {
+  const progress = useSharedValue<number>(0);
+  const [assets, error] = useAssets([
+    require("../assets/onboarding/images/notebook.png"),
+    require("../assets/onboarding/images/organization.png"),
+    require("../assets/onboarding/images/user-vision.png")
+  ]);
+
+  const onboardCarouselItems: OnboardCarouselItem[] = [
+    {
+      title: "Track Your Tasks",
+      description:
+        "Easily manage and organize your daily tasks with our intuitive interface.",
+      imagePath: assets?.[0].uri
+    },
+    {
+      title: "Stay Organized",
+      description: "Categorize your tasks and set priorities to maximize productivity.",
+      imagePath: assets?.[1].uri
+    },
+    {
+      title: "Achieve More",
+      description:
+        "Boost your efficiency and accomplish your goals with our task management system.",
+      imagePath: assets?.[2].uri
+    }
+  ];
+
+  if (error) {
+    console.error(error);
+    return (
+      <View>
+        <Text>Error loading assets</Text>
+      </View>
+    );
+  }
+
+  return (
+    <Carousel
+      autoPlayInterval={4000}
+      data={onboardCarouselItems}
+      loop={true}
+      pagingEnabled={true}
+      snapEnabled={true}
+      autoPlay={true}
+      onProgressChange={progress}
+      width={400}
+      height={400}
+      renderItem={({ item, index }) => {
+        return (
+          <View key={index} className='-ml-6 mt-4 w-full flex-1 items-center'>
+            <Image
+              source={{ uri: item.imagePath }}
+              className='aspect-square h-[260px] w-[260px] object-cover'
+            />
+            <View className='p-8'>
+              <Text className='text-center font-sans text-3xl font-medium leading-relaxed'>
+                {item.title}
+              </Text>
+              <Text className='text-md w-[280px] text-center font-sans leading-8'>
+                {item.description}
+              </Text>
+            </View>
+          </View>
+        );
+      }}
+    />
+  );
+}
