05d39f54eb4949be08879b61ebee5ccc7b595500
Kain Nhantumbo <nhantumbok@gmail.com>
2025-09-28 21:36:16 +0200

    feat: added new button and actions layout at onboarding screen

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,10 +1,14 @@
+import { Button } from "@/components/ui/button";
 import { Text } from "@/components/ui/text";
 import { appBaseConfig } from "@/constants";
+import { THEME } from "@/lib/theme";
 import { useAssets } from "expo-asset";
 import { LinearGradient } from "expo-linear-gradient";
 import { Link } from "expo-router";
+import { SproutIcon } from "lucide-react-native";
+import { useColorScheme } from "nativewind";
 import * as React from "react";
-import { Image, StatusBar, TouchableOpacity, View } from "react-native";
+import { Image, StatusBar, View } from "react-native";
 import { useSharedValue } from "react-native-reanimated";
 import Carousel from "react-native-reanimated-carousel";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -86,29 +90,62 @@ export function CarouselContainer() {
 }
 
 export default function Onboarding() {
+  const { colorScheme } = useColorScheme();
   return (
     <SafeAreaView className='flex-1'>
       <StatusBar barStyle='default' animated={true} backgroundColor={"#7e1b8c"} />
 
       <LinearGradient
-        colors={["#7e1b8c", "#ff6f91"]}
+        colors={["#7e1b8c", "#ff6f91", "#ff6f9110"]}
         style={{ flex: 1 }}
         start={{ x: 0, y: 0 }}
         end={{ x: 0, y: 1 }}>
         <CarouselContainer />
 
-        <View className='flex-1 items-center justify-center px-8'>
+        <View className='flex-1 items-center justify-center px-4'>
           <View className='mb-16'>
             <Text className='font-display text-5xl font-bold'>{appBaseConfig.title}</Text>
           </View>
 
-          <Link asChild href={"/login"}>
-            <TouchableOpacity
-              className='rounded-full bg-gray-800 px-12 py-4 shadow-lg'
-              activeOpacity={0.8}>
-              <Text className='font-sans text-lg font-medium text-white'>Get Started</Text>
-            </TouchableOpacity>
-          </Link>
+          <View className='flex w-full flex-col gap-3'>
+            <Link asChild href={"/(home)"}>
+              <Button size={"lg"} variant={"default"}>
+                <Text className='font-bold uppercase'>Skip and start now</Text>
+              </Button>
+            </Link>
+
+            <View className='flex w-full flex-row gap-2'>
+              <Link asChild href={"/login"} className='flex-1'>
+                <Button variant='outline' size='default' className='w-full'>
+                  <Text className='font-bold uppercase'>Login</Text>
+                </Button>
+              </Link>
+
+              <Link asChild href={"/signup"} className='flex-1'>
+                <Button variant='outline' size='default' className='w-full'>
+                  <Text className='font-bold uppercase'>Sign Up</Text>
+                </Button>
+              </Link>
+            </View>
+            <View className='flex flex-row items-center'>
+              <Link asChild href={"/signup"} className='flex-1'>
+                <Button variant='link' size='sm' className='w-full'>
+                  <Text className='text-sm font-semibold'>Terms of Use</Text>
+                </Button>
+              </Link>
+              <SproutIcon
+                size={18}
+                color={
+                  colorScheme === "light" ? THEME.light.foreground : THEME.dark.foreground
+                }
+              />
+              <Link asChild href={"/signup"} className='flex-1'>
+                <Button variant='link' size='sm' className='w-full'>
+                  <Text className='text-sm font-semibold'>Privacy Policy</Text>
+                </Button>
+              </Link>
+            </View>
+          </View>
         </View>
       </LinearGradient>
     </SafeAreaView>
