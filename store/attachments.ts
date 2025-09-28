b13e0b5cd3a901200728c44ccfb2a138755d9b4b
Kain Nhantumbo <nhantumbok@gmail.com>
2025-09-28 17:51:53 +0200

    feat: added getAttachmentsByNoteId helper function to attachments data store

--- store/attachments.ts
+++ store/attachments.ts
@@ -9,6 +9,7 @@ interface AttachmentsState {
   fetchAttachments: () => Promise<void>;
   addAttachment: (data: Omit<AttachmentSchemaType, "id">) => Promise<void>;
   deleteAttachment: (id: number, noteId: number) => Promise<void>;
+  getAttachmentsByNoteId: (noteId: number) => Promise<AttachmentSchemaType[]>;
 }
 
 export const useAttachmentsStore = create<AttachmentsState>((set, get) => ({
@@ -30,5 +31,10 @@ export const useAttachmentsStore = create<AttachmentsState>((set, get) => ({
       .delete(attachments)
       .where(and(eq(attachments.id, id), eq(attachments.noteId, noteId)));
     await get().fetchAttachments();
+  },
+
+  getAttachmentsByNoteId: async (noteId) => {
+    const data = await db.select().from(attachments).where(eq(attachments.noteId, noteId));
+    return data as AttachmentSchemaType[];
   }
 }));
